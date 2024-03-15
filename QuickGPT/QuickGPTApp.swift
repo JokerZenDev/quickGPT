64eba75fad03853830a5d6d8f9c32e507202d12c
JokerZenDev <michele.lenzi98@gmail.com>
2024-03-15 22:00:31 +0100

    Initial Commit

---
+++ QuickGPT/QuickGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickGPTApp.swift
+//  QuickGPT
+//
+//  Created by Michele Lenzi on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
